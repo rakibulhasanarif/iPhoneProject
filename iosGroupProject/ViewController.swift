b47647d7d66e13cae8331045324187fc95a87211
Rakibul Hasan <s3733338@student.rmit.edu.au>
2020-07-31 14:44:29 +1000

    Firrst Commit of The Project

---
+++ iosGroupProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosGroupProject
+//
+//  Created by Rakibul Hasan on 31/7/20.
+//  Copyright © 2020 Rakibul Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
